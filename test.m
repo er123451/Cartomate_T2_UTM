b4a306e9f3854d4a2e9cee49a73e7edb3b967f63
er123451 <ernestohontecillas@gmail.com>
2021-04-19 12:12:55 +0200

    corregida la x(paso directo), la y sigue sin funcionar

--- test.m
+++ test.m
@@ -1,1 +1,14 @@
-[x,y] = commands2.GKdir(52.336261,4.5,-3,0.9996,0,0,6378137,298.257222101)
+fid = fopen("1.txt","w");
+data = load("datos-practica-2.txt");
+a = data(1,1);
+invf = data(2,1);
+long0 = data(1,2);
+k0 = data(2,2);
+[m,n] = size(data);
+data1 = data(3:m,:);
+for i = 1:(m-3)
+    [x,y] = commands2.GKdir(data1(i,1),data1(i,2),long0,k0,0,0,a,invf);
+    fprintf(fid,'\n%.4f\t%.4f\n',x,y);
+end
+fclose(fid);
+
