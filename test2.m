9921bea4193cd61c626dc08a6da1953a41b3d16e
er <er@linux.local>
2021-05-01 23:13:05 +0200

    Código acabado, falta estilizar scripts

---
+++ test2.m
@@ -0,0 +1,17 @@
+fid = fopen("2.txt","w");
+data = load("1.txt");
+a = data(1,1);
+invf = data(2,1);
+long0 = data(1,2);
+k0 = data(2,2);
+[m,n] = size(data);
+data1 = data(3:m,1:2);
+data2 = data(1:2,1:2);
+fprintf(fid,'%.6f\t%.6f\n',data2(1,:));
+fprintf(fid,'%.6f\t%.6f\n',data2(2,:));
+for i = 1:(m-3)
+    [long,lat] = commands2.GKinv(data1(i,1),data1(i,2),long0,k0,0,0,a,invf);
+    fprintf(fid,'%.6f\t%.6f\n',long,lat);
+end
+
+fclose(fid);
